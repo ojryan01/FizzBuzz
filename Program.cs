016aad224984ae462e8434624c56b3c1cc864ebe
Olivia Ryan <74725067+ojryan01@users.noreply.github.com>
2021-10-25 15:57:23 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            for (int i = 1; i < 101; i++)
+
+            {
+                if ( (i % 3) == 0 && (i % 5) != 0)
+
+                {
+                    Console.WriteLine("Fizz");
+                }
+
+                else if ( (i % 5) == 0 && (i % 3) != 0)
+
+                {
+                    Console.WriteLine("Buzz");
+                }
+
+                else if ((i % 5) == 0 && (i % 3) == 0)
+
+                {
+                    Console.WriteLine("FizzBuzz");
+                
+                }
+
+                else
+                {
+                    Console.WriteLine(i);
+                }
+
+
+            }
+            
+            
+        }
+    }
+}
